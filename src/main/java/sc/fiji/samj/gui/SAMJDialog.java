799f8769dd142230d10f0e812d8ac1137a58d793
Vladimír Ulman <ulman@fi.muni.cz>
2024-01-18 16:13:35 +0100

    CHG2/3: SAMJDialog dropped handling of ROIs,...
    ...and communicates to PromptsResultsDisplay only

--- src/main/java/sc/fiji/samj/gui/SAMJDialog.java
+++ src/main/java/sc/fiji/samj/gui/SAMJDialog.java
@@ -2,7 +2,6 @@ package sc.fiji.samj.gui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-import java.awt.Rectangle;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
@@ -11,8 +10,6 @@ import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,20 +30,14 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.WindowManager;
 import ij.gui.GUI;
-import ij.gui.ImageCanvas;
-import ij.gui.PolygonRoi;
-import ij.gui.Roi;
-import ij.gui.Toolbar;
 import ij.process.ImageProcessor;
 import org.scijava.log.Logger;
+import sc.fiji.samj.communication.PromptsToNetAdapter;
 import sc.fiji.samj.communication.model.SAMModels;
-import sc.fiji.samj.gui.tools.SimulationSAM;
 import sc.fiji.samj.gui.tools.Tools;
 import sc.fiji.samj.ui.PromptsResultsDisplay;
 
-public class SAMJDialog extends JDialog implements ActionListener, MouseListener {
-
-	private int count = 0;
+public class SAMJDialog extends JDialog implements ActionListener {
 
 	private JButton bnClose = new JButton("Close");
 	private JButton bnHelp = new JButton("Help");
@@ -70,7 +61,6 @@ public class SAMJDialog extends JDialog implements ActionListener, MouseListener
 	private final SAMModelPanel panelModel;
 	private final PromptsResultsDisplay display;
 	private final Logger log;
-	private SAMRoiManager roiManager;
 
 	private boolean encodingDone = false;
 
@@ -81,11 +71,8 @@ public class SAMJDialog extends JDialog implements ActionListener, MouseListener
 		this.display = display;
 		this.log = hmmFijiLogForNow;
 
-
+		//TODO: is this needed?
 		this.imp = imp;
-		
-		ImageCanvas canvas = imp.getCanvas();
-		canvas.addMouseListener(this);
 
 		panelModel = new SAMModelPanel(availableModel);
 		// Buttons
@@ -152,29 +139,21 @@ public class SAMJDialog extends JDialog implements ActionListener, MouseListener
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == bnRect) {
-			IJ.setTool(Toolbar.RECT_ROI);
+			display.switchToUsingRectangles();
 		}
-		
 		if (e.getSource() == bnPoints) {
-			IJ.setTool(Toolbar.POINT);
+			display.switchToUsingPoints();
 		}
-		
 		if (e.getSource() == bnBrush) {
-			IJ.setTool(Toolbar.FREELINE);
+			display.switchToUsingLines();
 		}
-		
+
 		if (e.getSource() == bnHelp) {
 			Tools.help();
 		}
 		
 		if (e.getSource() == bnClose) {
-			if (imp != null) {
-				ImageCanvas canvas = imp.getCanvas();
-				if (canvas != null) {
-					canvas.removeMouseListener(this);
-				}
-			}
-			roiManager.close();
+			display.notifyNetToClose();
 			dispose();
 		}
 		
@@ -183,16 +162,22 @@ public class SAMJDialog extends JDialog implements ActionListener, MouseListener
 		}
 
 		if (e.getSource() == bnStart) {
+			if (!panelModel.getSelectedModel().isInstalled())
+				log.warn("Not starting encoding as the selected model is not installed.");
+
 			IJ.log("TO DO Start the encoding");
 			try {
+				PromptsToNetAdapter netAdapter = panelModel.getSelectedModel().instantiate(log);
+				//TODO: if this netAdapter has already encoded, we don't do it again
+				display.switchToThisNet(netAdapter);
 				Thread.sleep(500);
 			}
 			catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
 			IJ.log("TO DO End the encoding");
+			//TODO: encoding should be a property of a model
 			encodingDone = true;
-			roiManager = new SAMRoiManager(imp);
 		}
 		updateInterface();
 	}
@@ -202,54 +187,11 @@ public class SAMJDialog extends JDialog implements ActionListener, MouseListener
 		bnPoints.setEnabled(encodingDone);
 		bnBrush.setEnabled(encodingDone);
 		bnMask.setEnabled(encodingDone);
-		
-		bnComplete.setEnabled(roiManager.getCount() > 0);
-		bnRoi2Mask.setEnabled(roiManager.getCount() > 0);
-	}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
+		//TODO: this was checking if ROIManager is not empty...
+		bnComplete.setEnabled(false);
+		bnRoi2Mask.setEnabled(false);
 	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		Roi roi = imp.getRoi();
-		if (chkROIManager.isSelected())
-		if (roi != null) {
-			int py = this.getLocation().y + this.getSize().width;
-			int px = this.getLocation().x;
-			
-			Rectangle rect = roi.getBounds();
-			IJ.log(rect.toString());
-			double chrono = System.nanoTime();
-			SimulationSAM sam = new SimulationSAM(imp, rect);
-			sam.run();
-			PolygonRoi proi = sam.getResultAsRoi();
-			chrono = System.nanoTime() - chrono;
-			IJ.log(" len: " +  proi.getContainedPoints().length + " score: " + String.format("%3.3f", sam.getResultScore() )  + " time: " + String.format("%3.3f", (chrono / 1000000)) + " ms</small><br>");			//new PolygonRoi(p, PolygonRoi.POLYGON);
-			proi.setName("SAM-" + (++count));
-			roiManager.add(proi);
-			imp.setRoi(proi);
-			imp.updateAndRepaintWindow();
-			//roiManager.setLocation(new Point(px, py));
-		}
-		updateInterface();
-	}
-	
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// Handle mouse enter event
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// Handle mouse exit event
-	}
-
 
 	public class LocalDropTarget extends DropTarget {
 
