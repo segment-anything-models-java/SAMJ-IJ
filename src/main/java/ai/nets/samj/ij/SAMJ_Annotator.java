f1641be9b16c2e64277ebcba116d337d98fcaa14
Vladimír Ulman <ulman@fi.muni.cz>
2024-04-22 15:58:43 +0200

    CHG: SAMJDialog draws itself from the very beginning:
    - does own pack() etc
    - is based on ImageJ2

--- src/main/java/ai/nets/samj/ij/SAMJ_Annotator.java
+++ src/main/java/ai/nets/samj/ij/SAMJ_Annotator.java
@@ -19,25 +19,17 @@
  */
 package ai.nets.samj.ij;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
 import ai.nets.samj.communication.model.SAMModels;
 import ai.nets.samj.gui.SAMJDialog;
+import ai.nets.samj.ij.ui.IJSamjLogger;
 import ai.nets.samj.ui.SAMJLogger;
-import ij.ImageJ;
 import ij.ImagePlus;
-import ij.gui.GUI;
-import ij.plugin.PlugIn;
 import ai.nets.samj.ij.ui.IJ1PromptsProvider;
 import ai.nets.samj.ij.ui.IJSamMethods;
-
-// TODO I (Carlos) don't know how to develop in IJ2 @Plugin(type = Command.class, menuPath = "Plugins>SAMJ>Annotator")
-//TODO I (Carlos) don't know how to develop in IJ2 public class Plugin_SamJAnnotator implements Command {
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
 
 /**
  * ImageJ plugin that implements the SAMJ default GUI that can help annotating images 
@@ -45,65 +37,31 @@ import ai.nets.samj.ij.ui.IJSamMethods;
  * @author Carlos Garcia
  * @author Vladimir Ulman
  */
-public class SAMJ_Annotator implements PlugIn {
-	final static long MAX_IMAGE_SIZE_IN_BYTES = ((long)4)<<30; //4 GB
+@Plugin(type = Command.class, menuPath = "Plugins>SAMJ>Annotator")
+public class SAMJ_Annotator implements Command {
 
-	// TODO I (Carlos) don't know how to develop in IJ2 @Parameter
-	//private LogService logService = new LogService();
+	@Parameter
+	private LogService logService;
 
-	/**
-	 * Run the plugin
-	 */
 	public void run() {
-
-		// TODO I (Carlos) don't know how to develop in IJ2 final Logger log = logService.subLogger("SAMJ");
 		try {
-
 			//get list of recognized installations of SAM(s)
+			//TODO: why this is not part of SAMJ (application agnostic package)
 			final SAMModels availableModels = new SAMModels();
-			
-			// TODO I (Carlos) don't know how to develop in IJ2 Logger guiSublogger = log.subLogger("PromptsResults window");
-			SAMJLogger guilogger = new SAMJLogger() {
-				@Override
-				public void info(String text) {System.out.println(text);}
-				@Override
-				public void warn(String text) {System.out.println(text);}
-				@Override
-				public void error(String text) {System.out.println(text);}
-			};
 
-			// TODO I (Carlos) don't know how to develop in IJ2 Logger networkSublogger = log.subLogger("Networks window");
-			SAMJLogger networkLogger = new SAMJLogger() {
-				@Override
-				public void info(String text) {System.out.println("network -- " + text);}
-				@Override
-				public void warn(String text) {System.out.println("network -- " + text);}
-				@Override
-				public void error(String text) {System.out.println("network -- " + text);}
-			};
+			//get our own IJ2 loggers
+			final SAMJLogger guiLogger = new IJSamjLogger(logService.subLogger("SAMJ"));
+			final SAMJLogger networkLogger = new IJSamjLogger(logService.subLogger("Networks window"), "network -- ");
 
-
-			SwingUtilities.invokeLater(() -> {
-				SAMJDialog samjDialog = new SAMJDialog( availableModels, new IJSamMethods(), guilogger, networkLogger);
-				//create the GUI adapter between the user inputs/prompts and SAMJ outputs
-				samjDialog.setPromptsProvider((obj) -> {return new IJ1PromptsProvider((ImagePlus) obj, null);});// TODO log.subLogger("PromptsResults window"));});
-
-				JDialog dialog = new JDialog(new JFrame(), "SAMJ Annotator");
-				dialog.addWindowListener(new WindowAdapter() {
-					@Override
-					public void windowClosing(WindowEvent e) {
-						samjDialog.close();
-					}
-				});
-				dialog.add(samjDialog);
-				dialog.pack();
-				dialog.setResizable(false);
-				dialog.setModal(false);
-				dialog.setVisible(true);
-				GUI.center(dialog);
-		});
+			final SAMJDialog samjDialog
+					= new SAMJDialog(availableModels, new IJSamMethods(), guiLogger, networkLogger);
+			//create the GUI adapter between the user inputs/prompts and SAMJ outputs
+			samjDialog.setPromptsProvider(
+					  (obj) -> new IJ1PromptsProvider((ImagePlus) obj, logService.subLogger("PromptsResults window"))
+			);
+			//samjDialog.close();
 		} catch (RuntimeException e) {
-			//TODO log.error("SAMJ error: "+e.getMessage());
+			logService.error("SAMJ error: " + e.getMessage());
 			e.printStackTrace();
 		}
 	}
@@ -116,12 +74,5 @@ public class SAMJ_Annotator implements PlugIn {
 	public static void main(String[] args) {
 		ImageJ ij = new ImageJ();
 		new SAMJ_Annotator().run();
-	}
-
-
-	@Override
-	public void run(String arg) {
-		run();
-		
 	}
 }
