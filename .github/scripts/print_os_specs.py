bcc7fba561a5db043973b2afac1991428590fea4
carlosuc3m <100329787@alumnos.uc3m.es>
2025-06-03 10:48:09 +0200

    check where the installation is successfull

---
+++ .github/scripts/print_os_specs.py
@@ -0,0 +1,9 @@
+import java.lang
+
+
+os_name = java.lang.System.getProperty("os.name")
+os_version = java.lang.System.getProperty("os.version")
+
+
+print("Operating System: " + os_name)
+print("OS Version:       " + os_version)
