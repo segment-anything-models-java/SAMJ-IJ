0f31cd6241cc4bb8d76ed0e247e9dbd711436fdf
carlosuc3m <100329787@alumnos.uc3m.es>
2025-06-03 18:54:54 +0200

    fix jyhton script

--- .github/scripts/run_model.py
+++ .github/scripts/run_model.py
@@ -2,10 +2,11 @@ from ai.nets.samj.communication.model import SAM2Tiny
 from ai.nets.samj.ij import SAMJ_Annotator
 
 from java.util import ArrayList
+
 from time import time
 
 from ij import IJ
-
+from jarray import array
 from net.imglib2.img.display.imagej import ImageJFunctions
 
 
@@ -15,20 +16,25 @@ wrapImg = ImageJFunctions.convertFloat(blobs)
 model = SAM2Tiny()
 
 point_prompt = ArrayList()
-point_prompt.add((100, 100))
+
+
+my_seq = (100, 100)
+
+arr1 = array(my_seq,'i')
+
+
+point_prompt.add(arr1)
 
 start_time = time()
-
 mask = SAMJ_Annotator.samJReturnMask(model, wrapImg, point_prompt, None)
-
 end_time = time()
 
 mask_sum = 0
 cursor = mask.cursor()
 
 while cursor.hasNext():
-    mask_sum += cursor.get()
-    cursor.next()
+	cursor.next()
+	mask_sum += cursor.get().getRealDouble()
 
 print("Total non-zero pixels: " + str(mask_sum))
 print("Total time: " + str(end_time - start_time))
