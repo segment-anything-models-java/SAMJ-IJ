d9692a4f1d20e026465434791e6b16d59e6511c8
carlosuc3m <100329787@alumnos.uc3m.es>
2025-06-03 18:34:31 +0200

    add script to run model

--- .github/scripts/help_script.py
+++
@@ -1,7 +0,0 @@
-from java.io import File
-
-
-created = File("models").mkdir()
-
-print("created " + str(created))
-print("exists: " + str(File("models").exists()))
---
+++ .github/scripts/run_model.py
@@ -0,0 +1,34 @@
+from ai.nets.samj.communication.model import SAM2Tiny
+from ai.nets.samj.ij import SAMJ_Annotator
+
+from java.util import ArrayList
+from time import time
+
+from ij import IJ
+
+from net.imglib2.img.display.imagej import ImageJFunctions
+
+
+blobs = IJ.openImage("https://imagej.net/images/blobs.gif")
+wrapImg = ImageJFunctions.convertFloat(imp)
+
+model = SAM2Tiny()
+
+point_prompt = ArrayList()
+point_prompt.add((100, 100))
+
+start_time = time()
+
+mask = SAMJ_Annotator.samJReturnMask(model, wrapImg, point_prompt, None)
+
+end_time = time()
+
+mask_sum = 0
+cursor = mask.cursor()
+
+while cursor.hasNext():
+    mask_sum += cursor.get()
+    cursor.next()
+
+print("Total non-zero pixels: " + str(mask_sum))
+print("Total time: " + str(end_time - start_time))
