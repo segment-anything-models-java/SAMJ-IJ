d0e318aa8b60fd2e85b7edd4589de2525253e0ac
carlosuc3m <100329787@alumnos.uc3m.es>
2025-06-04 16:37:18 +0200

    correct the place where sam model is installed

--- .github/scripts/install_model.py
+++ .github/scripts/install_model.py
@@ -1,12 +1,25 @@
 from ai.nets.samj.communication.model import SAM2Tiny
 from java.lang import System
 from java.util.function import Consumer
+from deepimagej import Constants
+
+from io.bioimage.modelrunner.system import PlatformDetection
+
+from java.io import File
+import os
+
 
 class PrintConsumer(Consumer):
     def accept(self, s):
         System.out.println(s)
 
-model = SAM2Tiny()
+if PlatformDetection.isUsingRosseta() or PlatformDetection.getArch() == PlatformDetection.ARCH_ARM64:
+        relative_mamba = "appose_" + PlatformDetection.ARCH_ARM64
+else:
+        relative_mamba = "appose_" + PlatformDetection.getArch()
+
+
+model = SAM2Tiny(os.path.join(Constants.FIJI_FOLDER, relative_mamba))
 model.getInstallationManger().setConsumer(PrintConsumer());
 model.getInstallationManger().installEverything();
 
