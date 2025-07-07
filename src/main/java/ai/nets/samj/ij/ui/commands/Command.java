d43b343c440a691052af22ccd0afd46327f7e6bc
carlosuc3m <100329787@alumnos.uc3m.es>
2025-07-07 18:46:53 +0200

    add license headers

--- src/main/java/ai/nets/samj/ij/ui/commands/Command.java
+++ src/main/java/ai/nets/samj/ij/ui/commands/Command.java
@@ -1,3 +1,22 @@
+/*-
+ * #%L
+ * Plugin to help image annotation with SAM-based Deep Learning models
+ * %%
+ * Copyright (C) 2024 SAMJ developers.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package ai.nets.samj.ij.ui.commands;
 
 import java.util.List;
