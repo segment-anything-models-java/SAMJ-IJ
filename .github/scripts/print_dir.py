bc091a9644f362b216eb81ea67bc6c77289108ad
carlosuc3m <100329787@alumnos.uc3m.es>
2025-10-11 14:40:11 +0200

    add installer

--- .github/scripts/print_dir.py
+++ .github/scripts/print_dir.py
@@ -2,6 +2,9 @@ from ai.nets.samj.ij.utils import Constants
 from java.io import File
 from io.bioimage.modelrunner.system import PlatformDetection
 
+from ai.nets.samj.install import SamEnvManagerAbstract
+from ai.nets.samj.communication.model import SAM2Tiny
+
 
 if not PlatformDetection.isMacOS() or not PlatformDetection.isUsingRosseta():
     platform = PlatformDetection.getArch()
@@ -13,3 +16,9 @@ else:
 default_dir = Constants.FIJI_FOLDER + File.separator + "appose_" + platform
 
 print(default_dir)
+
+SamEnvManagerAbstract.DEFAULT_DIR = default_dir
+
+SAM2Tiny().getInstallationManger().installEverything()
+
+print("everything installed")
