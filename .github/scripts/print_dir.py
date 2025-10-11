d813cceeed4a4fcb61bdc90a814fe31614ea7919
carlosuc3m <100329787@alumnos.uc3m.es>
2025-10-11 14:20:36 +0200

    try another script

--- .github/scripts/print_dir.py
+++ .github/scripts/print_dir.py
@@ -1,3 +1,15 @@
 from ai.nets.samj.ij.utils import Constants
+from java.io import File
+from io.bioimage.modelrunner.system import PlatformDetection
 
-print(Constants.FIJI_FOLDER)
+
+if not PlatformDetection.isMacOS() or not PlatformDetection.isUsingRosseta():
+    platform = PlatformDetection.getArch()
+else:
+    platform = PlatformDetection.ARCH_ARM64
+
+
+
+default_dir = Constants.FIJI_FOLDER + File.separator + "appose_" + platform
+
+print(default_dir)
